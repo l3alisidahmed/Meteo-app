c0d014fca164a87902c0daba27be601c0bf5cdce
l3alisidahmed <sidoabdelali06@gmail.com>
2024-01-03 17:11:34 +0100

    change the sturcture of Files and Folders

---
+++ client/src/main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 1800,
+        height: 1500,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('D:/My Project/my-elctron-app/client/src/views/index.html')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
